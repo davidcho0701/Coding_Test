d023395ac086be763e27bc1ec8a08ad9e72fc11d
조성빈 <davidcho0701@gmail.com>
2026-04-07 14:44:03 +0900

    [Bronze II] Title: OX퀴즈, Time: 192 ms, Memory: 17828 KB -BaekjoonHub

--- Java/백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ Java/백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -4,30 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 1. 테스트 케이스 개수 입력
         int t = sc.nextInt();
-        
-        for (int i = 0; i < t; i++) {
-            // 2. OX 문자열 읽기
+
+        for(int i=0; i<t; i++) {
             String s = sc.next();
-            
-            int totalScore = 0; // 최종 점수
-            int currentConsecutive = 0; // 현재 연속된 O의 개수
-            
-            // 3. 문자열을 하나씩 탐색
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == 'O') {
-                    currentConsecutive++; // O면 연속 횟수 증가
-                    totalScore += currentConsecutive; // 증가된 값을 점수에 합산
+
+            int totalScore = 0;
+            int currentOCount = 0;
+
+            for(int j=0; j<s.length(); j++) {
+                if(s.charAt(j) == 'O') {
+                    currentOCount++;
+                    totalScore += currentOCount;
                 } else {
-                    currentConsecutive = 0; // X면 연속 횟수 리셋
+                    currentOCount = 0; //Point : 연속되고 있는 O 이 끝나면 리셋하여 연속추가할당 스톱시켜주기
                 }
             }
-            
-            // 4. 결과 출력
             System.out.println(totalScore);
         }
-        
         sc.close();
     }
 }
